0053d15927c011ef46849ecb91eaeac492cbd366
rhyanely <rhyanelycostaesilva@gmail.com>
2023-06-21 18:27:04 -0300

    inserindo comentarios no javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,13 +5,18 @@ const espacoImg = document.querySelectorAll('.col-2')
 const button = document.querySelector('.btn');
 
 // AJUSTANDO TAMANHO DAS IMAGENS
+
 imagem.forEach((imagem, index) => {
+
     imagem.style.maxWidth='150px';
     imagem.style.maxHeight='150px';
+
 });
 
-// AJUSTANDO DIVISÕES DE CADA IMAGEM 
+// AJUSTANDO CSS DAS DIVISÕES DE CADA IMAGEM 
+
 espacoImg.forEach((espacoImg, index) => {
+
     espacoImg.style='width: 150px';
     espacoImg.style='Height:150px';
     espacoImg.style.backgroundColor='gray';
@@ -20,20 +25,21 @@ espacoImg.forEach((espacoImg, index) => {
 });
 
 // INSERINDO A FUNCIONALIDADE DO BOTÃO 
+
 button.addEventListener("click", function(){
+
     imagem.forEach((imagem, index) => {
         imagem.classList.add('d-none');
     });
+
 });
 
 
-// INSERINDO FUNÇÃO DE CADA IMAGEM
-// espacoImg.addEventListener("click", function(){
-//     imagem[0].classlist.remove('d-none');
-// })
-
+// INSERINDO FUNCIONALIDADE AO CLICAR NA IMAGEM
 
 function exibir(numero){
-    let imagem = document.getElementById(numero)
-    imagem.className='img d-flex'
-}
+
+        let imagem = document.getElementById(numero)
+        imagem.className='img d-flex'
+
+ }
